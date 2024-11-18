24e9f547920ac40ad952c6efbc8a0b8b2ab57563
Adam S. <panebognik33@gmail.com>
2024-11-18 17:45:20 +0100

    Update blackjackv15_eng.c

--- blackjackv15_eng.c
+++ blackjackv15_eng.c
@@ -274,6 +274,7 @@ int dealer_round2(int *bank_value, int play_value, int *player_win, int *player_
     if (*bank_value + d1 + d2 < 14) {
         *bank_value = *bank_value + d1 + d2;
         printf("\n\nDealer: \"The bank takes %d points and rolls again...\"", d1 + d2);
+        dealer_round2(bank_value, play_value, player_win, player_loss);
         return *bank_value;
     }
     if (*bank_value + d1 < 17 && *bank_value + d2 < 17 && *bank_value + d1 + d2 >= play_value) {
